63aa5e261b24d6dfe9a19d04808220ee383dbbd5
JawadSarfraz <jawadsarfraz95@gmail.com>
2025-05-11 17:04:38 +0200

    Fixed data structure and dataset class implementation in training script for 20k dataset

--- experiments/01_increase_data_size/train_model_20k.py
+++ experiments/01_increase_data_size/train_model_20k.py
@@ -1,9 +1,8 @@
 import os
 import torch
-from transformers import XLMRobertaForSequenceClassification, XLMRobertaTokenizer, TrainingArguments, Trainer
+from transformers import XLMRobertaForSequenceClassification, TrainingArguments, Trainer
 from sklearn.metrics import f1_score, precision_score, recall_score, hamming_loss
 import numpy as np
-import json
 
 # Paths
 DATA_DIR = "experiments/01_increase_data_size/processed/"
@@ -15,13 +14,39 @@ input_ids = torch.load(os.path.join(DATA_DIR, "input_ids.pt"))
 attention_masks = torch.load(os.path.join(DATA_DIR, "attention_masks.pt"))
 labels = torch.load(os.path.join(DATA_DIR, "labels.pt"))
 
+# Dataset Class
+class SubjectDataset(torch.utils.data.Dataset):
+    def __init__(self, input_ids, attention_masks, labels):
+        self.input_ids = input_ids
+        self.attention_masks = attention_masks
+        self.labels = labels
+
+    def __len__(self):
+        return len(self.input_ids)
+
+    def __getitem__(self, idx):
+        return {
+            "input_ids": self.input_ids[idx],
+            "attention_mask": self.attention_masks[idx],
+            "labels": self.labels[idx],
+        }
+
 # Split data
 dataset_size = len(input_ids)
 train_size = int(0.8 * dataset_size)
 val_size = dataset_size - train_size
 
-train_dataset = torch.utils.data.TensorDataset(input_ids[:train_size], attention_masks[:train_size], labels[:train_size])
-val_dataset = torch.utils.data.TensorDataset(input_ids[train_size:], attention_masks[train_size:], labels[train_size:])
+train_dataset = SubjectDataset(
+    input_ids[:train_size],
+    attention_masks[:train_size],
+    labels[:train_size],
+)
+
+val_dataset = SubjectDataset(
+    input_ids[train_size:],
+    attention_masks[train_size:],
+    labels[train_size:],
+)
 
 # Model
 MODEL_NAME = "xlm-roberta-base"
